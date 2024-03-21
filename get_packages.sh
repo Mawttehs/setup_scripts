574d15c5307eff865bfa0f585208a7d766efd80d
Mawttehs <mawttehs@gmail.com>
2024-03-22 00:55:20 +0100

    insatlled packages

--- get_packages.sh
+++ get_packages.sh
@@ -1,5 +1,5 @@
 
-installed_packages_list=$(pacman -Qe | awk '!/nvidia|amd|intel/{print $1}')
+installed_packages_list=$(pacman -Qe | awk '!/\y(nvidia|amd|intel|dkms)\y/{print $1}')
 
 echo $installed_packages_list > installed_packages.txt
 
